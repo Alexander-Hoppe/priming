116f5a128e34812b93767a8a20ce52de7594c4ea
Karl-Alexander Hoppe <alexander.hoppe@tum.de>
2021-02-07 10:14:48 +0100

    added decision logic based on fake website input and a threshhold, meaningful names, removed unwanted code

--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
-let changeColor = document.getElementById("changeColor");
+let checkPolarity = document.getElementById("checkPolarity");
 
-chrome.storage.sync.get("color", ({ color }) => {
-	changeColor.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+// 	changeColor.style.backgroundColor = color;
+// });
 	
 // When the button is clicked, inject setPageBackGroundColor into current page
-changeColor.addEventListener("click", async () => {
+checkPolarity.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+	
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
 		function: setPageBackgroundColor
 	});
+
+	let websiteInput = .8;
+	let threshhold = evaluatePolarity(websiteInput);
+	if (1 > threshhold && threshhold > .5) {
+		let string = "Polarizing";
+		document.getElementById("output").innerHTML = string;
+	}
+	else if (.5 > threshhold && threshhold > 0){
+		let string = "Not polarizing";
+		document.getElementById("output").innerHTML = string;
+	}
+	else {console.log("Threshhold not in valid range")}
 });
 
 // The body of this function will be executed as a content script inside the 
@@ -21,6 +33,3 @@ function setPageBackgroundColor() {
 		document.body.style.backgroundColor = color;
 	});
 }
-
-const string = "test"
-document.getElementById("output").innerHTML = string;
