51d62b7d68af024d8640e622568a68130a3d23b0
Alexander Hoppe <hoppe397@gmail.com>
2021-02-07 21:48:47 +0100

    Got the domain and added switch case for domains, got html content of tab

--- popup.js
+++ popup.js
@@ -7,14 +7,61 @@ let checkPolarity = document.getElementById("checkPolarity");
 // When the button is clicked, inject setPageBackGroundColor into current page
 checkPolarity.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
 		function: setPageBackgroundColor
 	});
 
-	let websiteInput = .8;
-	let threshhold = evaluatePolarity(websiteInput);
+	// Grab the tab's url to build the twitter, fb, insta, snap, reddit, 4chan, etc. switch case
+	let domain = (new URL(tab.url));
+	let websiteInput, threshold;
+
+	chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			function: scrapeSource
+		},
+	    	(injectionResults) => {
+      			for (const frameResult of injectionResults)
+				document.getElementById("output").innerHTML = frameResult.result; //Printing the contents of the current tab into the popup, hand this over to website content next
+    		});
+
+	switch (domain.hostname) {
+		case "4chan.org":
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break; 
+		case "www.facebook.com": // ranked #2, de-de.facebook.com etc. will not work
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		case "www.instagram.com": // ranked #4
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		case "reddit.com": // ranked #3, old.reddit.com will be missing here
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		case "snapchat":
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		case "www.twitch.tv": // ranked #5
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		case "twitter.com": // ranked #6
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		case "www.youtube.com": // ranked #1
+			websiteInput = .8;
+			threshhold = evaluatePolarity(websiteInput);
+			break;
+		default:
+			threshhold = 0 // should deactivate all output instead
+	}
+	
 	if (1 > threshhold && threshhold > .5) {
 		let string = "Polarizing";
 		document.getElementById("output").innerHTML = string;
@@ -32,4 +79,9 @@ function setPageBackgroundColor() {
 	chrome.storage.sync.get("color", ({ color }) => {
 		document.body.style.backgroundColor = color;
 	});
+}
+
+// Get the html source contents of the url in the current tab
+function scrapeSource() {
+	return document.documentElement.outerHTML;
 }
