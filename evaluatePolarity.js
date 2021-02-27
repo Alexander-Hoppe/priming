833139c9b84617bf2dc99e3b03fdde80e3800c25
Alexander Hoppe <hoppe397@gmail.com>
2021-02-27 14:39:07 +0100

    pushed website html content into evaluatePolarity for further analysis

--- evaluatePolarity.js
+++ evaluatePolarity.js
@@ -1,4 +1,6 @@
 function evaluatePolarity(websiteInput){
+	// document.getElementById("output").innerHTML = websiteInput; //Printing the contents of the current tab into the popup
+	document.getElementById("output").innerHTML = typeof websiteInput;
 	let score = .2 * websiteInput;
 	return score;
 }
--- popup.js
+++ popup.js
@@ -14,63 +14,66 @@ checkPolarity.addEventListener("click", async () => {
 
 	// Grab the tab's url to build the twitter, fb, insta, snap, reddit, 4chan, etc. switch case
 	let domain = (new URL(tab.url));
-	let websiteInput, threshold;
-
+	let threshold; //websiteInput, threshold;
+	// let testWebsiteInput;
 	chrome.scripting.executeScript({
 			target: { tabId: tab.id },
 			function: scrapeSource
 		},
 	    	(injectionResults) => {
-      			for (const frameResult of injectionResults)
-				document.getElementById("output").innerHTML = frameResult.result; //Printing the contents of the current tab into the popup, hand this over to website content next
+      			//for (const frameResult of injectionResults)
+			//.... = frameResult.result;
+
+			let websiteInput = injectionResults[0].result;
+
+			switch (domain.hostname) {
+				case "4chan.org":
+					//websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break; 
+				case "www.facebook.com": // ranked #2, de-de.facebook.com etc. will not work
+					//websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				case "www.instagram.com": // ranked #4
+					websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				case "reddit.com": // ranked #3, old.reddit.com will be missing here
+					websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				case "snapchat":
+					websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				case "www.twitch.tv": // ranked #5
+					websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				case "twitter.com": // ranked #6
+					websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				case "www.youtube.com": // ranked #1
+					websiteInput = .8;
+					threshhold = evaluatePolarity(websiteInput);
+					break;
+				default:
+					threshhold = 0 // should deactivate all output instead
+			}
+			
+			if (1 > threshhold && threshhold > .5) {
+				let string = "Polarizing";
+				document.getElementById("output").innerHTML = string;
+			}
+			else if (.5 > threshhold && threshhold > 0){
+				let string = "Not polarizing";
+				document.getElementById("output").innerHTML = string;
+			}
+			else {console.log("Threshhold not in valid range")}
     		});
 
-	switch (domain.hostname) {
-		case "4chan.org":
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break; 
-		case "www.facebook.com": // ranked #2, de-de.facebook.com etc. will not work
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		case "www.instagram.com": // ranked #4
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		case "reddit.com": // ranked #3, old.reddit.com will be missing here
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		case "snapchat":
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		case "www.twitch.tv": // ranked #5
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		case "twitter.com": // ranked #6
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		case "www.youtube.com": // ranked #1
-			websiteInput = .8;
-			threshhold = evaluatePolarity(websiteInput);
-			break;
-		default:
-			threshhold = 0 // should deactivate all output instead
-	}
-	
-	if (1 > threshhold && threshhold > .5) {
-		let string = "Polarizing";
-		document.getElementById("output").innerHTML = string;
-	}
-	else if (.5 > threshhold && threshhold > 0){
-		let string = "Not polarizing";
-		document.getElementById("output").innerHTML = string;
-	}
-	else {console.log("Threshhold not in valid range")}
 });
 
 // The body of this function will be executed as a content script inside the 
