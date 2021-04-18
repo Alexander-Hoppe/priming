12f81fc5cadd94596d31476fb6421e98f5c074e3
Alexander Hoppe <hoppe397@gmail.com>
2021-04-18 18:34:07 +0200

    added Import, updated comments, handling no-match case

--- evaluatePolarity.js
+++ evaluatePolarity.js
@@ -1,24 +1,28 @@
+// import org.junit.Assert.assertTrue;
+
 function evaluatePolarity(websiteInput){
 	// assert that the input type is correct
-	assertTrue(typeof(websiteInput)==string)
+	// assertTrue(typeof(websiteInput)==string);
+	
 	// look for words like news, religion, politics, a.m.o. TODO in the string
-	// make a .json of words to look for and read it here
 	let counter = 0
 	if (websiteInput.includes("news") || websiteInput.includes("religion") || websiteInput.includes("politics")) {
 	    counter += 1;
 	}
 	// look for emojis in the string
 	// found at https://stackoverflow.com/questions/37089427/javascript-find-emoji-in-string-and-parse
-	// var unified_emoji_ranges = ['\ud83c[\udf00-\udfff]','\ud83d[\udc00-\ude4f]','\ud83d[\ude80-\udeff]'];
+	var unified_emoji_ranges = ['\ud83c[\udf00-\udfff]','\ud83d[\udc00-\ude4f]','\ud83d[\ude80-\udeff]'];
 
-	// var reg = new RegExp(unified_emoji_ranges.join('|'), 'g');
+	var reg = new RegExp(unified_emoji_ranges.join('|'), 'g');
 
-	// var string = "It is a great day ??.";
+	// change to websiteInput later
+	var string = "It is a great day ??.";
 
-	// if (string.match(reg)) {
-	//     alert("emoji found");
-        counter += 1000
-	// }
+	if (string.match(reg)) {
+	    alert("emoji found");
+            counter += 1000
+	}
+	else {counter += 100}
 
 	// document.getElementById("output").innerHTML = websiteInput; //Printing the contents of the current tab into the popup
 	document.getElementById("output").innerHTML = typeof websiteInput;
