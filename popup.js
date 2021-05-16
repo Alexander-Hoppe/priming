ba64ebadfdff1644cd60c173c2d6e5912ed53c79
Karl-Alexander Hoppe <hoppe397@gmail.com>
2021-05-16 16:21:07 +0200

    removed code related to changing color of page background

--- popup.js
+++ popup.js
@@ -1,16 +1,8 @@
 let checkPolarity = document.getElementById("checkPolarity");
 
-// chrome.storage.sync.get("color", ({ color }) => {
-// 	changeColor.style.backgroundColor = color;
-// });
-
 // When the button is clicked, inject setPageBackGroundColor into current page
 checkPolarity.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: setPageBackgroundColor
-	});
 
 	// Grab the tab's url to build the twitter, fb, insta, snap, reddit, 4chan, etc. switch case
 	let domain = (new URL(tab.url));
@@ -68,25 +60,19 @@ checkPolarity.addEventListener("click", async () => {
 			if (1 > threshhold && threshhold > .5) {
 				let string = "Polarizing";
 				document.getElementById("output").innerHTML = string;
+                document.body.style.backgroundColor = 'red';
                 // alert(string);
 			}
 			else if (.5 > threshhold && threshhold > 0){
 				let string = "Not polarizing";
 				document.getElementById("output").innerHTML = string;
+                document.body.style.backgroundColor = 'green';
                 // alert(string);
 			}
 			else {console.log("Threshhold not in valid range")}
     		});
 
 });
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-	chrome.storage.sync.get("color", ({ color }) => {
-		document.body.style.backgroundColor = color;
-	});
-}
 
 // Get the html source contents of the url in the current tab
 function scrapeSource() {
