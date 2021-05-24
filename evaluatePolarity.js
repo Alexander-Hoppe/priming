c350a009cfaf69f5360564c66b2fc72713351585
Karl-Alexander Hoppe <hoppe397@gmail.com>
2021-05-24 14:23:00 +0200

    removed naive evaluation

--- evaluatePolarity.js
+++ evaluatePolarity.js
@@ -1,18 +1,5 @@
 function evaluatePolarity(domain, websiteInput){
     console.assert(typeof(websiteInput) == "string", { websiteInput });
-    // look for words like news, religion, politics, a.m.o. TODO in the string
-    let counter = 0
-    if (websiteInput.includes("news") || websiteInput.includes("religion") || websiteInput.includes("politics")) {
-        counter += .1;
-    }
-
-    // look for emojis in the string
-    // found at https://stackoverflow.com/questions/37089427/javascript-find-emoji-in-string-and-parse
-    var unified_emoji_ranges = ['\ud83c[\udf00-\udfff]','\ud83d[\udc00-\ude4f]','\ud83d[\ude80-\udeff]'];
-    var reg = new RegExp(unified_emoji_ranges.join('|'), 'g');
-    if (websiteInput.match(reg)) {
-            counter += .033;
-    }
 
     // This only works when viewing a tweet with its response, i.e. when
     // "status" is in the twitter url
